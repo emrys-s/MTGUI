4cffe6205bc7089c6def162397a55aba2b5b734c
Ivan Dostovalov <i.dostovalov@gmail.com>
2016-12-07 15:57:44 +0200

    Deleted a lot of unnecessary data

--- mainwindow.h
+++ mainwindow.h
@@ -8,7 +8,6 @@ class QActionGroup;
 class QLabel;
 class QMenu;
 
-
 namespace Ui {
 class MainWindow;
 }
@@ -22,25 +21,12 @@ public:
     ~MainWindow();
 
 private slots:
-    void newFile();
-    void open();
-    void save();
-    void print();
-    void undo();
-    void redo();
-    void cut();
-    void copy();
-    void paste();
-    void bold();
-    void italic();
-    void leftAlign();
-    void rightAlign();
-    void justify();
-    void center();
-    void setLineSpacing();
-    void setParagraphSpacing();
+    void connectToRouter();
+    void importConfig();
+    void exportConfig();
+    void editConDet();
     void about();
-    void aboutQt();
+//    void aboutQt();
 
 private:
     void createActions();
@@ -57,28 +43,27 @@ private:
     QMenu *formatMenu;
 //    QMenu *helpMenu;
     QActionGroup *alignmentGroup;
-    QAction *newAct;
-    QAction *openAct;
-    QAction *saveAct;
-    QAction *printAct;
+    QAction *connectAct;
+    QAction *importAct;
+    QAction *exportAct;
+    QAction *editConDetAct;
     QAction *exitAct;
-    QAction *undoAct;
-    QAction *redoAct;
-    QAction *cutAct;
-    QAction *copyAct;
-    QAction *pasteAct;
-    QAction *boldAct;
-    QAction *italicAct;
-    QAction *leftAlignAct;
-    QAction *rightAlignAct;
-    QAction *justifyAct;
-    QAction *centerAct;
-    QAction *setLineSpacingAct;
-    QAction *setParagraphSpacingAct;
+//    QAction *undoAct;
+//    QAction *redoAct;
+//    QAction *cutAct;
+//    QAction *copyAct;
+//    QAction *pasteAct;
+//    QAction *boldAct;
+//    QAction *italicAct;
+//    QAction *leftAlignAct;
+//    QAction *rightAlignAct;
+//    QAction *justifyAct;
+//    QAction *centerAct;
+//    QAction *setLineSpacingAct;
+//    QAction *setParagraphSpacingAct;
     QAction *aboutAct;
-    QAction *aboutQtAct;
+//    QAction *aboutQtAct;
     QLabel *infoLabel;
-
 };
 
 #endif // MAINWINDOW_H
