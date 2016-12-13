80f66473c2f8df2385782d800501077f6553d5e9
Ivan Dostovalov <i.dostovalov@gmail.com>
2016-12-13 13:37:13 +0200

    Make some cleanup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "configuration.h"
-//#include <mikrotik-api.h>
 #include <mikrotikapi.h>
 #include <unistd.h>
 #include<sys/types.h>
@@ -14,8 +13,8 @@
 #include<iostream>
 
 #define SECONDS 30
-#define BANDWITH 100000
-//#define BANDWITH 100
+//#define BANDWITH 100000
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -30,34 +29,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     MikrotikAPI mt("94.45.71.39","nazar","ostware",8728);
 
- //   std::string szIPaddr = "94.45.71.39";
-//    char *szIPaddr = (char*)"94.45.71.39";
-//    char szIPaddr[] = {'9','4','.','4','5','.','7','1','.','3','9','\0'};
-//    int iPort=8728;
-//    char *szUsername = (char*)"nazar";
-//    char szUsername[] = {'n','a','z','a','r','2','\0'};
-
-//    char* szPassword = "";
-//    char szPassword[] = {'\0'};
-//    char *szPassword = (char*)"ostware";
-//    char szPassword[10] = {'o','s','t','w','a','r','e','\0'};
-
- //   char cWordInput[256];
- //   strcpy(cWordInput,(char*)"/interface/monitor-traffic");
-//    (char*)"/interface/monitor-traffic";
-//    struct Sentence stSentence;
-//    struct Block stBlock;
     QVector<double> x(SECONDS+1), y(SECONDS+1), z(SECONDS+1);
-
- //   int fdSock = ::apiConnect(szIPaddr.c_str(), iPort);
- //   int fdSock = ::apiConnect(szIPaddr, iPort);
- //   int iLoginResult = ::login(fdSock, szUsername, szPassword);
-
-//    if (!iLoginResult)
-//    {
-//         ::apiDisconnect(fdSock);
-//         printf("Invalid username or password.\n");
-//    }
 
     Sentence getIfaceStat;
 
@@ -74,20 +46,9 @@ MainWindow::MainWindow(QWidget *parent) :
         getIfaceStat.AddWord("=once=");
         getIfaceStat.AddWord("=interface=ether1");
         getIfaceStat.AddWord("\0");
-//        getIfaceStat.Print();
 
         mt.WriteSentence(getIfaceStat);
         mt.ReadBlock(ifaceStatus);
-//        ifaceStatus.Print();
-
-//        ifaceStatus.
-
-//        if (stSentence.iLength > 0)
-//        {
-//            writeSentence(fdSock, &stSentence);
-            // receive and print response block from the API
-//            stBlock = readBlock(fdSock);
-//        for (int i = 0; i < ifaceStatus[ifaceStatus.Length()]; i++)
         for (int i = 0; i < ifaceStatus.Length(); i++)
             {
                 for (int j = 0; j < ifaceStatus[i].Length() ; j++)
@@ -95,50 +56,20 @@ MainWindow::MainWindow(QWidget *parent) :
 //                    std::cout <<  ifaceStatus[i][j];
                     if (ifaceStatus[i][j].compare(0,19,"=tx-bits-per-second") == 0)
                     {
-                        std::cout << '\n' << ifaceStatus[i][j] << endl;
+ //                       std::cout << '\n' << ifaceStatus[i][j] << endl;
                         y[k] = atof(ifaceStatus[i][j].c_str()+20);
-                        std::cout << '\n' << y[k];
+ //                       std::cout << '\n' << y[k];
                     }
                     else if (ifaceStatus[i][j].compare(0,19,"=rx-bits-per-second") == 0)
                     {
-                        std::cout <<'\n' << ifaceStatus[i][j] << endl;
+//                        std::cout <<'\n' << ifaceStatus[i][j] << endl;
                         z[k] = atof(ifaceStatus[i][j].c_str()+20);
                     }
-//                        printf("\n***My print: %s",ifaceStatus[i][j]);
-//                    if (!strncmp(ifaceStatus[i][j],"=tx-bits-per-second",19))
-//                    {
-//                        printf("\n%s",stBlock.stSentence[i]->szSentence[j]+20);
-//                        y[k] = atof(stBlock.stSentence[i]->szSentence[j]+20);
-//                    }
-//                    else if (!strncmp(stBlock.stSentence[i]->szSentence[j],"=rx-bits-per-second",19))
-//                        z[k] = atof(stBlock.stSentence[i]->szSentence[j]+20);
                 }
             }
-
- /*          for (int i = 0; i < stBlock.iLength; i++)
-           {
-                 for (int j = 0; j < stBlock.stSentence[i]->iLength; j++)
-                 {
-                      if (!strncmp(stBlock.stSentence[i]->szSentence[j],"=tx-bits-per-second",19))
-                      {
-                          printf("\n%s",stBlock.stSentence[i]->szSentence[j]+20);
-                          y[k] = atof(stBlock.stSentence[i]->szSentence[j]+20);
-                      }
-                      else if (!strncmp(stBlock.stSentence[i]->szSentence[j],"=rx-bits-per-second",19))
-                        z[k] = atof(stBlock.stSentence[i]->szSentence[j]+20);
-                  }
-             }
-*/
-  //             clearSentence(&stSentence);
         x[k] = k;
         sleep(1);
-    //     y[k] = 60 + rand()%40;
-    //     z[k] = 60 + rand()%40;
     }
-//    for (int k=0; k<=SECONDS; k++)
-//    {
-//        printf("\nx[]:%3d y[]:%3d z[]:%3d",x[k],y[k],z[k]);
-//    }
 
     ui->widget_1->addGraph();
     ui->widget_1->xAxis->setLabel("time(in seconds)");
